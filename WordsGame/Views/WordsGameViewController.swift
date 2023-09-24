bc422816f3860397c30003bf2df5d7d6fb8bf694
mariamhovhannisyan <mariam.hovhannisyan@picsart.com>
2023-09-24 17:16:24 +0400

    add unit tests, optimize the logic, fix bug of not regenerating the list.

--- WordsGame/Views/WordsGameViewController.swift
+++ WordsGame/Views/WordsGameViewController.swift
@@ -16,7 +16,7 @@ class WordsGameViewController: UIViewController {
     @IBOutlet weak var wrongButton: UIButton!
     
     lazy var presenter = WordsGamePresenter(with: self)
-    let buttonWidth = CGFloat(100)
+    var isGameInProgress = false
     
     var spanishWordLabel = UILabel()
     
@@ -37,7 +37,13 @@ class WordsGameViewController: UIViewController {
         self.spanishWordLabel.backgroundColor = .gray
         self.spanishWordLabel.layer.borderColor = UIColor.black.cgColor
         self.spanishWordLabel.layer.borderWidth = 1
-        self.presenter.resetTheState()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if (!self.isGameInProgress) {
+            self.presenter.startNewGame()
+            self.isGameInProgress = true
+        }
     }
     
     @IBAction func correctButtonClicked(_ sender: Any) {
@@ -51,24 +57,16 @@ class WordsGameViewController: UIViewController {
 
 extension WordsGameViewController: PresenterView {
     func showSuccessEmoji() {
-        let image = UIImage(named: "correct")
-        let imageView = UIImageView(image:image)
-        let sizeConstant = CGFloat(40)
-        imageView.frame.size = CGSize(width: sizeConstant, height: sizeConstant)
-        self.view.addSubview(imageView)
-        imageView.center = self.view.center
-        imageView.center.y -= sizeConstant
-        imageView.contentMode = .scaleAspectFit
-        UIView.animate(withDuration: 1, delay: 0, options: [.curveLinear]) {
-            imageView.alpha = 0
-            imageView.center.y += sizeConstant/2
-        } completion: { _ in
-            imageView.removeFromSuperview()
-        }
+        self.showEmojiBasedOnAnswer(isCorrect: true)
     }
     
     func showFailEmoji() {
-        let image = UIImage(named: "wrong")
+        self.showEmojiBasedOnAnswer(isCorrect: false)
+    }
+    
+    private func showEmojiBasedOnAnswer(isCorrect: Bool) {
+        let imageName = isCorrect ? "correct" : "wrong"
+        let image = UIImage(named: imageName)
         let imageView = UIImageView(image:image)
         let sizeConstant = CGFloat(40)
         imageView.frame.size = CGSize(width: sizeConstant, height: sizeConstant)
@@ -92,25 +90,27 @@ extension WordsGameViewController: PresenterView {
         self.wrongAttemptsLabel.text = "Wrong attempts: \(number)"
     }
     
-    func stopTheGame(correctCount: Int, wrongCount: Int) {
+    func stopTheGame(correctCount: Int, wrongCount: Int, isWin: Bool) {
         self.spanishWordLabel.layer.removeAllAnimations()
-        if wrongCount == 3 {
+        if !isWin {
             let alert = UIAlertController(title: "Better luck next time !", message: "Correct attempts: \(correctCount) \n Wrong attempts: \(wrongCount)", preferredStyle: .alert)
             let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { action in
-                self.presenter.resetTheState()
+                self.presenter.startNewGame()
             })
             alert.addAction(restartAction)
             alert.addAction(UIAlertAction(title: "Close app", style: .default, handler: { action in
                 exit(0)
             }))
             present(alert, animated: true)
-        } else if wrongCount + correctCount == 15 {
+        } else {
             let alert = UIAlertController(title: "Let's try another 15 pairs !", message: "orrect attempts: \(correctCount) \n Wrong attempts: \(wrongCount)", preferredStyle: .alert)
             let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { action in
-                self.presenter.resetTheState()
+                self.presenter.startNewGame()
             })
             alert.addAction(restartAction)
-            alert.addAction(UIAlertAction(title: "Close app", style: .cancel))
+            alert.addAction(UIAlertAction(title: "Close app", style: .default, handler: { action in
+                exit(0)
+            }))
             present(alert, animated: true)
         }
         
@@ -120,10 +120,10 @@ extension WordsGameViewController: PresenterView {
         self.spanishWordLabel.layer.removeAllAnimations()
         self.englishWordLabel.text = self.presenter.getCurrentEnglishWord()
         self.spanishWordLabel.text = self.presenter.getCurrentSpanishWord()
-        self.spanishWordLabel.frame = CGRect(x: -buttonWidth*2 , y: -buttonWidth, width: buttonWidth*2, height: buttonWidth/2)
+        self.spanishWordLabel.frame = CGRect(x: -self.englishWordLabel.bounds.width , y: self.englishWordLabel.bounds.height, width: self.englishWordLabel.bounds.width, height: self.englishWordLabel.bounds.height)
         self.spanishWordLabel.center.x = self.view.center.x
         UIView.animate(withDuration: 5, delay: 0, options: [.curveLinear], animations: {
-            self.spanishWordLabel.center.y += self.view.bounds.height + self.buttonWidth*2
+            self.spanishWordLabel.center.y += self.view.bounds.height + self.englishWordLabel.bounds.height*2
         }, completion: { (finished) in
             if finished {
                 self.presenter.userDidntGuessInTime()
